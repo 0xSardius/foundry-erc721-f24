f71eb841131d722438d501ffbcabfd189d1565b8
Sardius <0xsardius@gmail.com>
2024-10-25 06:55:45 -0400

    deploy and test made

---
+++ test/BasicNftTest.t.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+
+import {Test, console} from "forge-std/Test.sol";
+import {BasicNft} from "../src/BasicNft.sol";
+import {DeployBasicNft} from "../script/DeployBasicNft.s.sol";
+
+contract BasicNftTest is Test {
+    DeployBasicNft public deployer;
+    BasicNft public basicNft;
+    address public user = makeAddr("user");
+    string public constant PUG = "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    function setUp() public {
+        deployer = new DeployBasicNft();
+        basicNft = deployer.run();
+    }
+
+    function testNameIsCorrect() public view {
+        string memory expectedName = "Dogie";
+        string memory actualName = basicNft.name();
+        assertEq(keccak256(abi.encodePacked(actualName)), keccak256(abi.encodePacked(expectedName)));
+    }
+
+    function tesCanMintAndHaveABalance() public {
+        vm.prank(user);
+        basicNft.mintNft(PUG);
+        assertEq(basicNft.balanceOf(user), 1);
+        assertEq(keccak256(abi.encodePacked(PUG)), keccak256(abi.encodePacked(basicNft.tokenURI(0))));
+    }
+}
